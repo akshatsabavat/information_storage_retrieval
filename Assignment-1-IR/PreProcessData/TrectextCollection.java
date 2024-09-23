9f2efacd2ed1a0670e03ae1c6cf8d42be922ff79
akshatsabavat <akshat.sabavat@gmail.com>
2024-09-23 05:56:05 +0530

    Defined: Reading the TrecTextCollection preprocessor method

--- Assignment-1-IR/PreProcessData/TrectextCollection.java
+++ Assignment-1-IR/PreProcessData/TrectextCollection.java
@@ -1,7 +1,11 @@
 package PreProcessData;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import Classes.Path;
+
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,23 +14,50 @@ import java.util.Map;
  * Please add comments along with your code.
  */
 public class TrectextCollection implements DocumentCollection {
-	//you can add essential private methods or variables
+	private BufferedReader reader; // for reading documents line by line
+	private String linePointer; // points towards reading the line of any incomiung document
 
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public TrectextCollection() throws IOException {
-		// This constructor should open the file existing in Path.DataTextDir
-		// and also should make preparation for function nextDocument()
-		// you cannot load the whole corpus into memory here!!
-
+	reader = new BufferedReader(new FileReader(Path.DataTextDir));
+	linePointer = reader.readLine(); // starts the reading of the file
 	}
 
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public Map<String, Object> nextDocument() throws IOException {
-		// this method should load one document from the corpus, and return this document's number and content.
-		// the returned document should never be returned again.
-		// when no document left, return null
-		// NTT: remember to close the file that you opened, when you do not use it any more
+		// Reaching end of document check
+		if(linePointer == null) {
+			reader.close();
+			return null;
+		}
 
+		StringBuilder documentContentBuilder = new StringBuilder(); // accumalates and builds document content
+		String documentNumber = null; // string variable to store the document number associated with the documnet content
+
+		while(linePointer != null) {
+			if (linePointer.startsWith("<DOC>")) {
+				// Resetting whenever I come across a new document
+				documentNumber = null;
+				documentContentBuilder.setLength(0);
+		
+            } else if (linePointer.startsWith("<DOCNO>")){
+				// the line below skips over the 7 charecters of the <DOCNO /> and then it begins extraction of the document number till it reaches the ending tag
+				documentNumber = linePointer.substring(7, linePointer.indexOf("</DOCNO>")).trim(); // document number stored
+			} else if (linePointer.startsWith("</DOC>")) {
+				// Creates a new <Document Number, Documnet Content> structure and returns, if I have a document number
+				if(documentNumber != null) {
+					Map<String, Object> document = new HashMap<>(); 
+					document.put(documentNumber, documentContentBuilder.toString().toCharArray());
+					return document;
+				}
+			} else {
+				if (documentNumber != null) {
+					// Keeps building the document content, as the linePointer moves through the document
+					documentContentBuilder.append(linePointer).append(" ");
+				}
+			}
+			linePointer = reader.readLine();
+		}
 
 		return null;
 	}
